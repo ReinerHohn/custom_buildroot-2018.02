d5ce51ecae3335189cb54b9844565e948b31fbf3
Michael Dick <mi-dick@gmx.de>
2018-03-26 09:40:20 +0200

    Pigpio wird mit gebaut, rootfs wird gepackt und an 'RASP1' geschickt und wlan0 wird auf custom linux erkannt

---
+++ board/raspberrypi/create-toolch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -u
+set -e
+
+tar -czf /tmp/toolchain.tar.gz -C ${HOST_DIR} .
+sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no /tmp/toolchain.tar.gz pi@RASP1:
+sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no ${BINARIES_DIR}/sdcard.img pi@RASP1:
