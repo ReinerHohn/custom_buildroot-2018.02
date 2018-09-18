8b1227cbf8b4380c82371b666e516c287a719d88
Michael Dick <mi-dick@gmx.de>
2018-09-18 22:02:42 +0200

    gewnhywfar compiliert, aber Probleme beim Install wegen libtool

--- package/michael/gwenhywfar/gwenhywfar.mk
+++ package/michael/gwenhywfar/gwenhywfar.mk
@@ -7,11 +7,11 @@
 GWENHYWFAR_VERSION = 4.20.0 
 #GWENHYWFAR_SOURCE = 
 GWENHYWFAR_SITE = https://www.aquamaniac.de/sites/download/download.php?package=01\&release=208\&file=02\&dummy=gwenhywfar-4.20.0.tar.gz
-#GWENHYWFAR_SITE = https://git.aquamaniac.de/git/gwenhywfar 
 GWENHYWFAR_SITE_METHOD = wget
 GWENHYWFAR_INSTALL_STAGING = YES
 GWENHYWFAR_INSTALL_TARGET = NO
-GWENHYWFAR_CONF_OPTS = --prefix=$(STAGING_DIR)/usr --host=$(GNU_TARGET_NAME) --build=$(GNU_HOST_NAME) --disable-binreloc #--with-libiconv-prefix=$(STAGING_DIR)/usr
+GWENHYWFAR_CONV_ENV = "$(DESTDIR) = /tmp"
+GWENHYWFAR_CONF_OPTS = --disable-binreloc --with-openssl-includes=$(STAGING_DIR)/usr --with-guis='' --disable-static --prefix=/usr #--with-libiconv-prefix=$(STAGING_DIR)/usr
 GWENHYWFAR_DEPENDENCIES = #libglib2 host-pkgconf
 
 $(eval $(autotools-package))
