cfae793b6ae99ce0f54175243d8a65ce8fae394f
Michael Dick <mi-dick@gmx.de>
2018-09-20 22:12:51 +0200

    gwenhywfar compiliert mit

--- package/michael/gwenhywfar/gwenhywfar.mk
+++ package/michael/gwenhywfar/gwenhywfar.mk
@@ -8,10 +8,13 @@ GWENHYWFAR_VERSION = 4.20.0
 #GWENHYWFAR_SOURCE = 
 GWENHYWFAR_SITE = https://www.aquamaniac.de/sites/download/download.php?package=01\&release=208\&file=02\&dummy=gwenhywfar-4.20.0.tar.gz
 GWENHYWFAR_SITE_METHOD = wget
+# GWENHYWFAR_INSTALL_TARGET = YES
 GWENHYWFAR_INSTALL_STAGING = YES
-GWENHYWFAR_INSTALL_TARGET = NO
-GWENHYWFAR_CONV_ENV = "$(DESTDIR) = /tmp"
-GWENHYWFAR_CONF_OPTS = --disable-binreloc --with-openssl-includes=$(STAGING_DIR)/usr --with-guis='' --disable-static --prefix=/usr #--with-libiconv-prefix=$(STAGING_DIR)/usr
+
+GWENHYWFAR_MAKE_OPTS = prefix= exec_prefix= bindir=
+#GWENHYWFAR_INSTALL_STAGING_OPTS = prefix= exec_prefix= bindir=  # DESTDIR=$(STAGING_DIR) #prefix=/tmp #$(STAGING_DIR) 
+GWENHYWFAR_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR)  
+GWENHYWFAR_CONF_OPTS = --disable-binreloc --with-openssl-includes=$(STAGING_DIR)/usr --with-guis='' --disable-shared DESTDIR= --libdir=/tmp --bindir=/tmp  --prefix=/usr --exec_prefix=  #--with-libiconv-prefix=$(STAGING_DIR)/usr
 GWENHYWFAR_DEPENDENCIES = #libglib2 host-pkgconf
 
 $(eval $(autotools-package))
