7af136cca6592454385a1fe0355abe0c1f368002
Michael Dick <mi-dick@gmx.de>
2018-09-22 14:48:59 +0200

    Aqbanking compiliert cross, es gibt aber noch einen buildhänger

--- package/michael/gwenhywfar/gwenhywfar.mk
+++ package/michael/gwenhywfar/gwenhywfar.mk
@@ -5,16 +5,15 @@
 ################################################################################
 
 GWENHYWFAR_VERSION = 4.20.0 
-#GWENHYWFAR_SOURCE = 
 GWENHYWFAR_SITE = https://www.aquamaniac.de/sites/download/download.php?package=01\&release=208\&file=02\&dummy=gwenhywfar-4.20.0.tar.gz
 GWENHYWFAR_SITE_METHOD = wget
-# GWENHYWFAR_INSTALL_TARGET = YES
 GWENHYWFAR_INSTALL_STAGING = YES
+# GWENHYWFAR_INSTALL_TARGET = NO
 
-GWENHYWFAR_MAKE_OPTS = prefix= exec_prefix= bindir=
-#GWENHYWFAR_INSTALL_STAGING_OPTS = prefix= exec_prefix= bindir=  # DESTDIR=$(STAGING_DIR) #prefix=/tmp #$(STAGING_DIR) 
-GWENHYWFAR_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR)  
-GWENHYWFAR_CONF_OPTS = --disable-binreloc --with-openssl-includes=$(STAGING_DIR)/usr --with-guis='' --disable-shared DESTDIR= --libdir=/tmp --bindir=/tmp  --prefix=/usr --exec_prefix=  #--with-libiconv-prefix=$(STAGING_DIR)/usr
-GWENHYWFAR_DEPENDENCIES = #libglib2 host-pkgconf
+GWENHYWFAR_MAKE_OPTS= prefix=$(STAGING_DIR) exec_prefix=$(STAGING_DIR) bindir=$(STAGING_DIR)/usr/bin libdir=$(STAGING_DIR)/usr/lib
+#rrGWENHYWFAR_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR)/ install 
+GWENHYWFAR_CONF_OPTS = --disable-binreloc --with-openssl-includes=$(STAGING_DIR)/usr --with-guis='' #--prefix=$(STAGING_DIR)/usr
+#GWENHYWFAR_CONF_OPTS = --host=$(GNU_TARGET_NAME) --disable-binreloc --with-openssl-includes=$(STAGING_DIR)/usr --with-guis='' --prefix=/usr #--prefix=$(STAGING_DIR)/usr --exec_prefix=$(STAGING_DIR)/usr #--bindir=$(STAGING_DIR)/usr/bin --libdir=$(STAGING_DIR)/usr/lib  
+GWENHYWFAR_DEPENDENCIES = gnutls libgcrypt
 
 $(eval $(autotools-package))
