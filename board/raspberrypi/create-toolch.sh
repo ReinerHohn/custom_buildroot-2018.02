a14568366e0c91a4584ffb9acde358d6a378eb5e
Michael Dick <mi-dick@gmx.de>
2018-09-27 20:19:27 +0200

    Toolchain für Debuggen fit gemacht

--- board/raspberrypi/create-toolch.sh
+++ board/raspberrypi/create-toolch.sh
@@ -3,6 +3,6 @@
 set -u
 set -e
 
-tar -czf /tmp/toolchain.tar.gz -C ${HOST_DIR} .
-sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no /tmp/toolchain.tar.gz pi@RASP1:
-sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no ${BINARIES_DIR}/sdcard.img pi@RASP1:
+tar -czf ${HOST_DIR}/../images/toolchain.tar.gz -C ${HOST_DIR}/ .
+#sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no /tmp/toolchain.tar.gz pi@RASP1:
+#sshpass -p 'raspberry' scp -o StrictHostKeyChecking=no ${BINARIES_DIR}/sdcard.img pi@RASP1:
